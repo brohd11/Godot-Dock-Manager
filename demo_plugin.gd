414ff1a6d8d82003b5fb3b3c82327278bdcdc67e
brohd <brohdie_larocque@hotmail.com>
2025-11-28 05:15:28 -0800

    rework layout saving, add InstanceManager class

--- demo_plugin.gd
+++ demo_plugin.gd
@@ -1,72 +1,53 @@
 @tool
 extends EditorPlugin
 
-# you can place the dock_manager folder anywhere in plugin, then preload here
+# You can place the dock_manager folder anywhere in plugin, then preload here.
+# Alternatively, use as a global class and use with Plugin Exporter
 
-var dock_manager:DockManager
-var dock_manager2:DockManager
-var dock_manager3:DockManager
-
-var main_screen_handler: DockManager.MainScreenHandlerMulti
+var dm_ins_manager:DockManager.InstanceManager
 
 const GUI_SCENE = preload("res://addons/dock_manager/demo_gui.tscn")
 const OTHER_GUI = preload("res://addons/dock_manager/demo_other_gui.tscn")
 const THIRD_GUI = preload("res://addons/dock_manager/demo_third_gui.tscn")
 
-# must have
+# must have this function
 func _get_plugin_name() -> String:
 	return "Test Docking"
 
-# must have
+# if you want to use main screen, function must be present, along with _make_visible calling the instance manager
 func _has_main_screen() -> bool:
 	return true
 
+func _make_visible(visible: bool) -> void:
+	dm_ins_manager.on_plugin_make_visible(visible)
+
 # Can be used in single GUI mode. In multi GUI mode, docks will be named as the root of the scene and you can declare
-# variable "icon" in your scene script. See: "demo_gui.gd". _DockManager.new(self, gui, Slot, false), 4th argument sets GUI mode.
+# variable "icon" in your scene script.
 func _get_plugin_icon() -> Texture2D:
 	return EditorInterface.get_base_control().get_theme_icon("Node", &"EditorIcons")
 
-
 func _enter_tree() -> void:
-	main_screen_handler = DockManager.MainScreenHandlerMulti.new(self)
+	var load_freeable_docks = true # loads freeable docks that haven't been manually freed
+	var allow_single_instance = true # onlt allow a single instance of a scene to be instanced
+	dm_ins_manager = DockManager.InstanceManager.new(self, load_freeable_docks, allow_single_instance)
 	
-	var can_be_freed = false
-	dock_manager = DockManager.new(self, GUI_SCENE, dock_manager.Slot.BOTTOM_PANEL, can_be_freed, main_screen_handler)
-	# class needs reference to plugin and GUI, other params optional.
+	dm_ins_manager.new_persistent_dock_manager(GUI_SCENE, DockManager.Slot.BOTTOM_PANEL)
 	
 	add_tool_menu_item("Other GUI", _on_other_gui_tool_menu_pressed)
 	add_tool_menu_item("Third GUI", _on_third_gui_tool_menu_pressed)
 
 func _exit_tree() -> void:
-	dock_manager.clean_up() # frees GUI, saves layout
-	if is_instance_valid(dock_manager2):
-		dock_manager2.clean_up()
-	if is_instance_valid(dock_manager3):
-		dock_manager3.clean_up()
-	if is_instance_valid(main_screen_handler):
-		main_screen_handler.clean_up()
-		main_screen_handler.queue_free()
+	if is_instance_valid(dm_ins_manager):
+		dm_ins_manager.clean_up()
 	
 	remove_tool_menu_item("Other GUI")
 	remove_tool_menu_item("Third GUI")
 
 func _get_window_layout(configuration: ConfigFile) -> void:
-	dock_manager.save_layout_data() # saves layout everytime it is changed, vs on exit only
-	if is_instance_valid(dock_manager2):
-		dock_manager2.save_layout_data()
-	if is_instance_valid(dock_manager3):
-		dock_manager3.save_layout_data()
+	dm_ins_manager.save_layout_data() # saves layout everytime it is changed, vs on exit only
 
 func _on_other_gui_tool_menu_pressed():
-	if is_instance_valid(dock_manager2):
-		print("ALREADY INSTANCED: ", dock_manager2.plugin_control)
-		return
-	var can_be_freed = true
-	dock_manager2 = DockManager.new(self, OTHER_GUI, DockManager.Slot.BOTTOM_PANEL, can_be_freed, main_screen_handler)
+	dm_ins_manager.new_freeable_dock_manager(OTHER_GUI, DockManager.Slot.BOTTOM_PANEL)
 
 func _on_third_gui_tool_menu_pressed():
-	if is_instance_valid(dock_manager3):
-		print("ALREADY INSTANCED: ", dock_manager3.plugin_control)
-		return
-	var can_be_freed = true
-	dock_manager3 = DockManager.new(self, THIRD_GUI, DockManager.Slot.BOTTOM_PANEL, can_be_freed, main_screen_handler)
+	dm_ins_manager.new_freeable_dock_manager(THIRD_GUI, DockManager.Slot.BOTTOM_PANEL)
